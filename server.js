fddd37fd6e3be073c28d38bdf6e7ec6a0c274d38
Ryan Magoon <mr.magoon5@gmail.com>
2016-10-16 22:24:38 -0400

    update server to support mongoose

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 // modules
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // set our port
 var port = process.env.PORT || 3000;
 
-// set the static files location
+// set up mongoose, assume it is locally installed
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/RESTServer');
+
+// set the static files location for our Ember application
 app.use(express.static(__dirname + '/public'));
 
-require('./app/routes')(app); // configure our routes
+// bodyParser middleware to allow different encoding requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());  // to support JSON-encoded bodies
+
+// Routes API
+const router = express.Router();
+app.use('/', router);
+require('./app/routes')(router); // configure our routes
 
 // start up our app at http://localhost:3000
 app.listen(port);
